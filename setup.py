4205fe3317163a5e68791b6cb6933b2adcd1ca4b
enceladus-rex <>
2019-06-29 04:35:02 -0400

    Preparing for release

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
 from distutils.core import setup
+from os.path import dirname, join
 
 
 setup(
   name='webauthn-rp',
-  version='0.0',
+  version='0.0.1',
   description='Web Authentication Relying Party Library',
   author='enceladus-rex',
   packages=['webauthn_rp'],
+  url='https://github.com/enceladus-rex/webauthn-rp',
+  long_description=open(join(dirname(__file__), 'README.md')).read(),
+  long_description_content_type='text/markdown',
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ],
   install_requires=[
-    'cbor',
-    'cryptography',
-    'pyopenssl',
+    'cbor==1.0.0',
+    'cryptography==2.6.1',
   ],
 )
